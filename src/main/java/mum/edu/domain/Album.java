bf7dc8ed7bdb8bd6c952b5c83a49362a34d817df
Dane Savot <dnnddane@gmail.com>
2015-11-16 18:50:20 -0600

    Update dependencies, add hibernate and others.

--- src/main/java/mum/edu/domain/Album.java
+++ src/main/java/mum/edu/domain/Album.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+@Entity
 public class Album {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/mum/edu/domain/Favorite.java
+++ src/main/java/mum/edu/domain/Favorite.java
@@ -3,6 +3,7 @@ package mum.edu.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+@Entity
 public class Favorite {
 
 	@Id
