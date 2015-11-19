077435bc41fed8b692cf663a33e854680f17d1ac
Dane Savot <dnnddane@gmail.com>
2015-11-18 23:28:22 -0600

    Add population script

--- src/sql/populate.sql
+++ src/sql/populate.sql
@@ -3,7 +3,4 @@ INSERT INTO credentials(username,password,enabled,authority) VALUES ('dane','dan
 INSERT INTO credentials(username,password,enabled,authority) VALUES ('qin','qin', TRUE , 'admin');
 INSERT INTO credentials(username,password,enabled,authority) VALUES ('user','user', TRUE , 'user');
 
-INSERT INTO `member` (`email`, `firstName`, `lastName`, `credentials_username`) VALUES ('dane.savot@mum.edu', 'Dane', 'Savot', 'dane');
-INSERT INTO `member` (`email`, `firstName`, `lastName`, `credentials_username`) VALUES ('qin@mum.edu', 'Qin', 'Qin', 'qin');
-INSERT INTO `member` (`email`, `firstName`, `lastName`, `credentials_username`) VALUES ('user@gmail.com', 'User', 'User', 'user');
 
