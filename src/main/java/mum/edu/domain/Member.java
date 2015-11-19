c071ec5b70e9068b224a6b9330aee860e65449b1
Dane Savot <dnnddane@gmail.com>
2015-11-19 16:55:35 -0600

    Add population for member in Population.sql

--- src/main/java/mum/edu/domain/Member.java
+++ src/main/java/mum/edu/domain/Member.java
@@ -22,7 +22,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 public class Member {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	@NotEmpty(message = "{NotEmpty}")
