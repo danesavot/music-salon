df2ee170c1a1212ae284ff6b9ff90f3ccf03b71e
Dane Savot <dnnddane@gmail.com>
2015-11-19 16:31:27 -0600

    Update populate.sql to include population member also

--- src/sql/populate.sql
+++ src/sql/populate.sql
@@ -1,7 +1,11 @@
-
 INSERT INTO credentials(username,password,enabled,authority) VALUES ('dane','dane', TRUE, 'ROLE_ADMIN');
 INSERT INTO credentials(username,password,enabled,authority) VALUES ('qin','qin', TRUE , 'ROLE_ADMIN');
 INSERT INTO credentials(username,password,enabled,authority) VALUES ('rishi','rishi', TRUE , 'ROLE_ADMIN');
 INSERT INTO credentials(username,password,enabled,authority) VALUES ('user','user', TRUE , 'ROLE_USER');
+INSERT INTO `member` (`email`, `firstName`, `lastName`, `credentials_username`) VALUES ('dane.savot@mum.edu', 'Dane', 'Savot', 'dane');
+INSERT INTO `member` (`email`, `firstName`, `lastName`, `credentials_username`) VALUES ('qin@mum.edu', 'Qin', 'Qin', 'qin');
+INSERT INTO `member` (`email`, `firstName`, `lastName`, `credentials_username`) VALUES ('rishi@mum.edu', 'rishi', 'rishi', 'rishi');
+INSERT INTO `member` (`email`, `firstName`, `lastName`, `credentials_username`) VALUES ('user@mum.edu', 'user', 'user', 'user');
+
 
 
