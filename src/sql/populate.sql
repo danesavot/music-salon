0a733513f428a4f322763d2a109bf4248effa62e
Dane Savot <dnnddane@gmail.com>
2015-11-18 23:50:58 -0600

    Apply Role Based URL authorization

--- src/sql/populate.sql
+++ src/sql/populate.sql
@@ -1,6 +1,7 @@
 
-INSERT INTO credentials(username,password,enabled,authority) VALUES ('dane','dane', TRUE, 'admin');
-INSERT INTO credentials(username,password,enabled,authority) VALUES ('qin','qin', TRUE , 'admin');
-INSERT INTO credentials(username,password,enabled,authority) VALUES ('user','user', TRUE , 'user');
+INSERT INTO credentials(username,password,enabled,authority) VALUES ('dane','dane', TRUE, 'ROLE_ADMIN');
+INSERT INTO credentials(username,password,enabled,authority) VALUES ('qin','qin', TRUE , 'ROLE_ADMIN');
+INSERT INTO credentials(username,password,enabled,authority) VALUES ('rishi','rishi', TRUE , 'ROLE_ADMIN');
+INSERT INTO credentials(username,password,enabled,authority) VALUES ('user','user', TRUE , 'ROLE_USER');
 
 
